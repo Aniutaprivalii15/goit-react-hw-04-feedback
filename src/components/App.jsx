72a38efdf3f23fd6a9221fdc056c2d44339f5927
Anna <anytka141528@gmail.com>
2023-04-25 15:26:50 +0300

    fix all

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
+
 import { Statistics } from './Statistics/Statistics';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
